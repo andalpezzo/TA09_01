7a0367884fd35583cb1cea87ba657db6d6d9a81a
andalpezzo <andalpezzo@gmail.com>
2023-03-16 20:10:20 +0100

    First main

---
+++ src/Main/MainApp.java
@@ -0,0 +1,25 @@
+package Main;
+
+import Electrodomestico.Electrodomestico;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declaración de las variables
+			Object Electrodomesticos[] = new Object[10];
+			
+			Electrodomestico Nevera = Electrodomestico();
+			Electrodomestico Tostador = Electrodomestico();
+			Electrodomestico Microondas = Electrodomestico();
+			Electrodomestico Plancha = Electrodomestico();
+			Electrodomestico Cafetera = Electrodomestico();
+			Electrodomestico AireAcondicionado = Electrodomestico();
+			Electrodomestico Horno = Electrodomestico();
+			Electrodomestico Batidora = Electrodomestico();
+			Electrodomestico LavadoraSamsung = Electrodomestico();
+			Electrodomestico TelevisionLG = Electrodomestico();
+	}
+}
+
